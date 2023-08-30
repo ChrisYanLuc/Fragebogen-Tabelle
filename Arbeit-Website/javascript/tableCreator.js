5d00c47073b1ce88f1e6b9eb6cada3a516616bb4
ChrisYanLuc <anfrage.christian@gmx.de>
2023-08-30 12:13:29 +0200

    add row/col button
    now able to add rows and columns

--- Arbeit-Website/javascript/tableCreator.js
+++ Arbeit-Website/javascript/tableCreator.js
@@ -53,6 +53,16 @@ var tableData = [];
       tableData[row][col] = value;
     }
 
+    function updateRow(row) {
+      var table = document.getElementById("myTable");
+      var rowData = tableData[row];
+      var cells = table.rows[row + 1].cells;
+      for (var i = 0; i < rowData.length; i++) {
+        var value = cells[i].textContent.trim();
+        rowData[i] = value;
+      }
+    }
+
     function deleteRow() {
       var deletionInput = document.getElementById("deletionInput");
       var row = parseInt(deletionInput.value);
@@ -68,23 +78,69 @@ var tableData = [];
       var rowCount = table.childElementCount;
       
       for (var i = 0; i < rowCount; i++) {
-        tableData.splice(col, 1);
+        tableData.splice(col-1, 1);
       }
       for (var i = 0; i < rowCount; i++) {
-        table.rows[i].deleteCell(col);
+        table.rows[i].deleteCell(col-1);
      }
     }
 
-    function updateRow(row) {
+    function addRow() {
       var table = document.getElementById("myTable");
-      var rowData = tableData[row];
-      var cells = table.rows[row + 1].cells;
-      for (var i = 0; i < rowData.length; i++) {
-        var value = cells[i].textContent.trim();
-        rowData[i] = value;
+      var columns = tableData[0].length;
+      var rowData = [];
+      var row = document.createElement("tr");
+      for (var j = 0; j < columns; j++) {
+        var cell = document.createElement("td");
+        cell.setAttribute("contenteditable", "true");
+        cell.setAttribute("class", "editable-cell");
+        cell.addEventListener("input", updateCell);
+        rowData.push("");
+        row.appendChild(cell);
       }
+      table.appendChild(row);
+      tableData.push(rowData);
+    }
+
+    function addColumn() {
+      var table = document.getElementById("myTable");
+      var rows = tableData.length;
+      var columns = tableData[0].length;
+
+      // Create table header
+      var th = document.createElement("th");
+      var input = document.createElement("input");
+      input.setAttribute("type", "text");
+      input.setAttribute("class", "header-input");
+      input.setAttribute("placeholder", "New Column");
+      th.appendChild(input);
+
+      table.rows[0].appendChild(th);
+      
+      for (var i = 0; i < rows; i++) {
+        var row = table.rows[i+1];
+
+        var cell = document.createElement("td");
+        cell.setAttribute("contenteditable", "true");
+        cell.setAttribute("class", "editable-cell");
+        cell.addEventListener("input", updateCell);
+
+        tableData[i].push("");
+        row.appendChild(cell);
+      }
+
     }
 
     function displayData() {
       console.log(tableData);
+    }
+
+    function workinProgress() {
+      const popup = document.getElementById("popupWorkInProgress");
+      popup.style.visibility = 'visible';
+      setTimeout(hide, 2000, popup);
+    }
+
+    function hide(popup){
+      popup.style.visibility = 'hidden';
     }
