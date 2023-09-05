ec83bf53b0730397af79bb8420a0d94bb7a5c576
ChrisYanLuc <anfrage.christian@gmx.de>
2023-09-05 18:06:06 +0200

    load premade table
    - loading given table when window is loaded
    - fixed deleting-functions
    - update todo
    - update css buttons on the right

--- javascript/tableCreator.js
+++ javascript/tableCreator.js
@@ -43,7 +43,7 @@ var tableData = [];
     }
 
     function updateCell(event) {
-      var rowIndex = event.target.parentNode.rowIndex - 1;
+      var rowIndex = event.target.parentNode.rowIndex;
       var columnIndex = event.target.cellIndex;
       var value = event.target.textContent.trim();
       updateData(rowIndex, columnIndex, value);
@@ -67,22 +67,46 @@ var tableData = [];
       var deletionInput = document.getElementById("deletionInput");
       var row = parseInt(deletionInput.value);
       var table = document.getElementById("myTable");
+
+      if (isNaN(row)){
+        window.alert("Bitte gib eine Nummer größer als 0 und kleiner gleich " + (tableData.length-1) );
+        return;
+      } else if (!tableData[0]){
+        window.alert("Da ist gar keine Tabelle. Du hast wohl die ganze Tabelle gelöscht du Schlingel. Lade einfach die Seite neu.");
+        return;
+      } else if (row < 0 || row > tableData.length-1){
+        window.alert("Bitte gib eine Nummer größer als 0 und kleiner gleich " + (tableData.length-1) );
+        return;
+      }
+
       table.deleteRow(row);
       tableData.splice(row, 1);
     }
 
     function deleteColumn() {
+      var table = document.getElementById("myTable");
+      var rowCount = table.rows.length;
       var deletionInput = document.getElementById("deletionInput");
       var col = parseInt(deletionInput.value);
-      var table = document.getElementById("myTable");
-      var rowCount = table.childElementCount;
+      console.log(col);
+      if (isNaN(col)){
+        window.alert("Bitte gib eine Nummer größer als 0 und kleiner gleich " + (tableData[0].length-1) );
+        return;
+      } else if (!tableData[0]){
+        window.alert("Da ist gar keine Tabelle. Du hast wohl die ganze Tabelle gelöscht du Schlingel. Lade einfach die Seite neu.");
+        return;
+      } else if (col < 0 || col > tableData[0].length-1){
+        window.alert("Bitte gib eine Nummer größer als 0 und kleiner gleich " + (tableData[0].length-1) );
+        return;
+      }
       
       for (var i = 0; i < rowCount; i++) {
-        tableData.splice(col-1, 1);
+        tableData[i].splice(col, 1);
+        table.rows[i].deleteCell(col);
       }
-      for (var i = 0; i < rowCount; i++) {
-        table.rows[i].deleteCell(col-1);
-     }
+      if (tableData[0].length == 0){
+        tableData.splice(0, tableData.length);
+      }
     }
 
     function addRow() {
@@ -104,8 +128,9 @@ var tableData = [];
 
     function addColumn() {
       var table = document.getElementById("myTable");
-      var rows = tableData.length;
+      var rows = table.rows.length;
       var columns = tableData[0].length;
+      rowData = [];
 
       // Create table header
       var th = document.createElement("th");
@@ -115,10 +140,12 @@ var tableData = [];
       input.setAttribute("placeholder", "New Column");
       th.appendChild(input);
 
+      tableData[0].push("");
       table.rows[0].appendChild(th);
       
-      for (var i = 0; i < rows; i++) {
-        var row = table.rows[i+1];
+      // Create Editable cells in columns
+      for (var i = 1; i < rows; i++) {
+        var row = table.rows[i];
 
         var cell = document.createElement("td");
         cell.setAttribute("contenteditable", "true");
@@ -132,7 +159,14 @@ var tableData = [];
     }
 
     function displayData() {
+      console.log("Tabellen Daten: ------------------------------------------------------------");
+      for (i = 0; i < tableData.length; i++){
+        for (j = 0; j < tableData[i].length; j++){
+          console.log(tableData[i][j]);
+        }
+      }
       console.log(tableData);
+      
     }
 
     function workinProgress() {
@@ -143,4 +177,88 @@ var tableData = [];
 
     function hide(popup){
       popup.style.visibility = 'hidden';
+    }
+
+    function readTable(){
+      var table = document.getElementById("myTable");
+      for (var i=0; i<table.length; i++){
+        var rowData = []
+        for (var j=0; j<table.rows[i].length; j++){
+          rowData.push(table.rows[j].cell[j]);
+        }
+        tableData.push(rowData)
+      }
+      displayData();
+
+      //see line 56
+      var table = document.getElementById("myTable");
+      var rowData = tableData[row];
+      var cells = table.rows[row + 1].cells;
+      for (var i = 0; i < rowData.length; i++) {
+        var value = cells[i].textContent.trim();
+        rowData[i] = value;
+      }
+    }
+
+    function createPremadeTable(){
+      var table = document.getElementById("myTable");
+      var leitfragen = ["Was sollen die Schüler*innen wissen/können?", 
+      "Was gibt es darüber hinaus inhaltlich noch zu wissen? (Inhalte, die S*S jetzt (noch) nicht brauchen)", 
+      "Warum ist es wichtig, dass Schüler*innen genau das lernen?", 
+      "Welche Vorgaben werden im Rahmenlehrplan/ Schulinternen Curriculum gemacht?", 
+      "Welches Vorwissen besitzen die Schüler*innen zu diesem Thema?", 
+      "Welche (Fehl-) Vorstellungen gibt es zu dieser Idee?", 
+      "Welche Schwierigkeiten oder Einschränkungen im Zusammenhang mit der Vermittlung der Idee können auftreten?", 
+      "Durch welche Faktoren (z.B. Klassenklima, Lernumgebung, etc.) kann die Vermittlung des Inhalts noch beeinflusst werden? (positiv und negativ)", 
+      "Wie soll der Inhalt vermittelt werden? (z.B. Methoden, Vorgehen, Sozialformen, Experimente, Lernumgebung, etc.)", 
+      "Warum soll der Inhalt auf diese Weise vermittelt werden?", 
+      "Wie kann überprüft werden, was die Schüler*innen verstanden haben und wo es noch Verständnisschwierigkeiten gibt? (inkl. Bandbreite an möglichen Antworten)"];
+      
+      var columns = 3;
+
+      // Clear existing table
+      while (table.firstChild) {
+        table.removeChild(table.firstChild);
+      }
+
+      // Create table header
+      var rowData = [];
+      var headerRow = document.createElement("tr");
+      var th = document.createElement("th");
+      th.textContent = "Leitfragen ↴ | Big Ideas → ";
+      rowData.push(th.textContent);
+      headerRow.appendChild(th);
+      for (var i = 1; i < columns; i++) {
+        var th = document.createElement("th");
+        var input = document.createElement("input");
+        input.setAttribute("type", "text");
+        input.setAttribute("class", "header-input");
+        input.setAttribute("placeholder", "Big Idea " + i);
+        th.appendChild(input);
+        headerRow.appendChild(th);
+        rowData.push("");
+      }
+      table.appendChild(headerRow);
+      tableData.push(rowData)
+
+      // Create table rows
+      for (var i = 0; i < leitfragen.length; i++) {
+        var rowData = [];
+        var row = document.createElement("tr");
+        var cell = document.createElement("td");
+          cell.textContent = leitfragen[i];
+          rowData.push(cell.textContent);
+          row.appendChild(cell);
+        for (var j = 1; j < columns; j++) {
+          var cell = document.createElement("td");
+            cell.setAttribute("contenteditable", "true");
+            cell.setAttribute("class", "editable-cell");
+            cell.addEventListener("input", updateCell);
+          rowData.push("");
+          row.appendChild(cell);
+        }
+        table.appendChild(row);
+        tableData.push(rowData);
+      }
+
     }
